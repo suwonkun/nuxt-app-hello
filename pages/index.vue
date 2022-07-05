07e20b71143d7fd0873e97db614310fe44bc396c
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-07-05 20:36:29 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+      <p>{{ message }}</p>
+      <a href=""></a>
+  </section>
+</template>
+ 
+  
+<script>
+export default {
+  data: function(){
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
